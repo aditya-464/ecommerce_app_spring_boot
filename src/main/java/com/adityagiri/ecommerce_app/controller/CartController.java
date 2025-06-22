81c72be99e1c4040b2d61c419677c7154d770737
Aditya Giri <adityagiri1911@gmail.com>
2025-06-22 08:00:37 +0530

    CartController fixed

--- src/main/java/com/adityagiri/ecommerce_app/controller/CartController.java
+++ src/main/java/com/adityagiri/ecommerce_app/controller/CartController.java
@@ -2,21 +2,17 @@ package com.adityagiri.ecommerce_app.controller;
 
 import com.adityagiri.ecommerce_app.dto.cart.AddToCartRequestDTO;
 import com.adityagiri.ecommerce_app.dto.cart.CartResponseDTO;
-import com.adityagiri.ecommerce_app.entity.Cart;
 import com.adityagiri.ecommerce_app.service.CartService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/cart")
 public class CartController {
 
     private final CartService cartService;
-
     public CartController(CartService cartService) {
         this.cartService = cartService;
     }
